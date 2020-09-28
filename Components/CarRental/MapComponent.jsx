93884c9502fb56cfc9b8b1835441ac598bd32964
chamoddissanayake <dissanayakechamod@gmail.com>
2020-09-28 12:13:45 +0530

    Supplier Info component done

--- Components/CarRental/MapComponent.jsx
+++ Components/CarRental/MapComponent.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Button, Text, StyleSheet, View } from "react-native";
+import AppButton from "../../common/AppButton";
+import AppText from "../../common/AppText";
+import colors from "../../config/colors";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 export default class MapComponent extends Component {
   constructor(props) {
@@ -9,13 +14,116 @@ export default class MapComponent extends Component {
 
   render() {
     return (
-      <View>
-        <View>
-          <Text>Map Component</Text>
+      <View style={{ flex: 0.5 }}>
+        <View
+          style={[
+            styles.container,
+            {
+              // flexDirection: rowMode ? "row" : "column",
+              minHeight: 100,
+            },
+          ]}
+        >
+          <View style={{ paddingLeft: 10, paddingTop: 10 }}>
+            <AppText style={{ fontWeight: "bold" }}>Supplier Info</AppText>
+          </View>
+          <View style={styles.mapArea}>
+            <Text>Map is here</Text>
+            <Text></Text>
+          </View>
+
+          <View style={styles.supplierAddressContactButtonView}>
+            <View>
+              {/* Supplier address start */}
+              <View style={styles.supplierAddress}>
+                <AppText style={{ fontSize: 15 }}>
+                  65, Arrival Meeting Hall,
+                </AppText>
+                <AppText style={{ fontSize: 15 }}>Airport Boulevard</AppText>
+                <AppText style={{ fontSize: 15 }}>Singapore</AppText>
+                <AppText></AppText>
+              </View>
+              {/* Supplier address end */}
+            </View>
+            <View>
+              {/* Supplier contact button start */}
+              <View style={{ borderRadius: 20 }}>
+                <Button
+                  // onPress={onPressLearnMore}
+                  title="Chat with supplier >>>"
+                  color="#003580"
+                />
+              </View>
+              {/* Supplier contact button end */}
+            </View>
+          </View>
+
+          <View style={styles.addressPhonemailContainer}>
+            <View style={{ paddingLeft: 5 }}>
+              <AntDesign name="phone" size={20} />
+            </View>
+            <View>
+              <AppText style={{ fontSize: 15, paddingLeft: 10 }}>
+                +65123456789
+              </AppText>
+            </View>
+          </View>
+
+          <View style={styles.addressPhonemailContainer}>
+            <View style={{ paddingLeft: 5 }}>
+              <AntDesign name="mail" size={20} />
+            </View>
+            <View>
+              <AppText style={{ fontSize: 15, paddingLeft: 10 }}>
+                abc@abc.com
+              </AppText>
+            </View>
+          </View>
+
+          <View style={styles.addressPhonemailContainer}>
+            <View style={{ paddingLeft: 5 }}>
+              <Feather name="globe" size={20} />
+            </View>
+            <View>
+              <AppText style={{ fontSize: 15, paddingLeft: 10 }}>
+                http://www.abc.com
+              </AppText>
+            </View>
+          </View>
         </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    backgroundColor: colors.light,
+    flex: 1,
+    padding: 3,
+    borderRadius: 3,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+  mapArea: {},
+  supplierAddress: {},
+  supplierAddressContactButtonView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+  },
+  addressPhonemailContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    padding: 2,
+  },
+});
